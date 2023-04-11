91b079ab327761f8be48a5725ef02c92b3756265
Wallo <wallo@gmail.com>
2023-04-11 14:55:20 +0200

    update 11/04

--- src/FunzioniGeneraTAG.js
+++ src/FunzioniGeneraTAG.js
@@ -1,10 +1,62 @@
 function scriviTAG() {
-    let div = document.getElementById("idDivTAG")
-    let strListaTAG = generaTag("img", "goku.jpg", "idImg", "Classe", "img/goku.jpg" )
-    strListaTAG += generaTag("img", "goku.jpg", "", "Classe", "img/anime.gif" )
-    div.innerHTML = strListaTAG
+    let div = document.getElementById("idDivTAG");
+    //genera TAG da string = innerHTML
+    let strListaTAG = generaTag("img", "goku.jpg", "idImg", "Classe", "img/goku.jpg" );
+    strListaTAG += generaTag("img", "goku.jpg", "", "Classe", "img/anime.gif" );
+    div.innerHTML = strListaTAG;
+    //genera TAG da Obj = appendChild()
+    let elementoCreato = generaTagObject("img", "goku.jpg", "idImg", "Classe", "img/goku.jpg");
+    div.appendChild(elementoCreato);
 
 }
+
+function scriviTagObj() {
+    let div = document.getElementById("idInsertElementObj");
+    //genera TAG da Obj = appendChild()
+    let elementoCreato = generaTagObject("img", "goku.jpg", "idImg", "Classe", "img/goku.jpg");
+    div.appendChild(elementoCreato);
+    elementoCreato = generaTagObject("img", "goku.jpg", "idGif", "Classe", "img/anime.gif" );
+    div.appendChild(elementoCreato)
+
+}
+function generaTagObject(tag, innerText, id, classe, url) {
+    //let stringaTag = "";
+    let elemento = null;
+    let lowerTag = tag.toLowerCase();
+    elemento = document.createElement(lowerTag)
+    switch (lowerTag) {
+        case 'p' :
+        case 'h1' :
+        case 'h3' :
+        case 'span' :
+            //elemento = document.createElement(lowerTag);
+            elemento.setAttribute("id", id);
+            elemento.setAttribute("class", classe);
+            elemento.textContent = innerText
+            
+        case 'a' :              
+            //elemento = document.createElement(lowerTag);
+            elemento.setAttribute("id", id);
+            elemento.setAttribute("src", url);
+            elemento.setAttribute("class", classe);
+            elemento.textContent = innerText;
+            break;
+        case 'img' :                   
+            //elemento = document.createElement(lowerTag);
+            elemento.setAttribute("id", id);
+            elemento.setAttribute("src", url);
+            elemento.setAttribute("class", classe);
+            elemento.setAttribute("alt", innerText);
+            break;
+            
+            default:
+                elemento = null
+                break;
+            }
+    console.log(elemento);   
+    return elemento
+}
+
 function generaTag(tag, innerText, id, classe, url) {
     let stringaTag = "";
     let lowerTag = tag.toLowerCase();
